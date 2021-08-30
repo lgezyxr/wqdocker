286fe65ecad61bbb139f546030c10af008d8dd34
Muyi <lgezyxr@gmail.com>
2021-08-30 14:26:37 -0400

    Update entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -7,9 +7,9 @@ echo "serving frontend"
 #if [ "$DEBUG" = 1 ]
 #then
     echo "develompent running frontend"
-    npm run start
+#    npm run start
 #else
 #    echo "productions running frontend"
 #    serve build -d -l 3000
 #fi
-# DANGEROUSLY_DISABLE_HOST_CHECK=true HOST=0.0.0.0 npm start
+DANGEROUSLY_DISABLE_HOST_CHECK=true HOST=0.0.0.0 npm start
