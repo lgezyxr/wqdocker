92e29f35275c27b86742853b91c8dd2bcff86f92
Muyi <lgezyxr@gmail.com>
2021-09-28 10:17:32 +0800

    Update entrypoint.sh
    123

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,9 +2,10 @@
 export PYTHONUNBUFFERED=TRUE
 mkdir -p /logs
 service statd start
+python manage.py makemigrations --merge 
 python image_similarity/main.py 2>&1 | tee /logs/gunicorn_image_similarity.log &
 python manage.py showmigrations | tee /logs/show_migrate.log
-python manage.py makemigrations --merge | tee /logs/show_migrate.log
+
 python manage.py migrate | tee /logs/command_migrate.log
 python manage.py showmigrations | tee /logs/show_migrate.log
 python manage.py build_similarity_index 2>&1 | tee /logs/command_build_similarity_index.log
